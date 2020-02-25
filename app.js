bb271c4bb24e5f489114c52795030dc97f31b978
dakaras <dan.karagiannis@protonmail.ch>
2020-02-24 21:34:23 -0600

    finished problem

--- app.js
+++ app.js
@@ -12,3 +12,14 @@
 //sample input = 1,2,3,4,5
 //sample output = 10 14
 
+function minMaxSum(arr){
+    let newArr = [...arr].sort() //spread operator keeps arr from being manipulated
+    let sum = 0
+
+    for(let i = 0; i < newArr.length; i++){
+        sum += newArr[i]
+    }
+    let max = sum - newArr[0]
+    let min = sum - newArr[newArr.length-1]
+    console.log(min, max)
+}
